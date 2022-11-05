8c48957082222a5d754cb7b39601ef672f71f426
Andreas Urbanski <11169240+nardeas@users.noreply.github.com>
2022-11-05 13:35:52 +0200

    Add package setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mishmash',
+    version='0.0.1',
+    packages=['api'],
+    description='api package',
+    author='Team Glados',
+    author_email='andreas.urbanski@gmail.com',
+    python_requires='>=3.9.0',
+    license='TBD',
+    install_requires=[],
+    classifiers=[
+        'License :: Other/Proprietary License',
+        'Private :: Do Not Upload to pypi server',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
