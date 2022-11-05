490c00792ff31c8c46425b1b0bb371958db7646f
Ville Toiviainen <villej.toiviainen@gmail.com>
2022-11-05 15:28:06 +0200

    improve api

--- api/routes/text_to_speech.py
+++ api/routes/text_to_speech.py
@@ -7,6 +7,7 @@ import concurrent
 import functools
 import os
 from google.cloud import texttospeech
+from enum import Enum
 
 
 def google_text_to_speech(api_key, **kwargs):
@@ -14,19 +15,19 @@ def google_text_to_speech(api_key, **kwargs):
         client = texttospeech.TextToSpeechClient.from_service_account_file(api_key)
 
         prompt = kwargs["prompt"]
-        gender = kwargs["gender"].lower()
+        gender = kwargs["gender"]
         language_code = kwargs["language_code"]
 
         synthesis_input = texttospeech.SynthesisInput(text=prompt)
 
-        if "male" in gender:
+        if gender == Gender.MALE:
             voice_gender = texttospeech.SsmlVoiceGender.MALE
-        elif "female" in gender:
+        elif gender == Gender.FEMALE:
             voice_gender = texttospeech.SsmlVoiceGender.FEMALE
         else:
             voice_gender = texttospeech.SsmlVoiceGender.NEUTRAL
         voice = texttospeech.VoiceSelectionParams(
-            language_code=language_code, ssml_gender=voice_gender
+            language_code=language_code, ssml_gender=voice_gender, name="Wavenet-A"
         )
         audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
         response = client.synthesize_speech(
@@ -49,8 +50,27 @@ async def text_to_speech(*args, **kwargs):
         )
 
 
+class LangugeCode(str, Enum):
+    ENG_US = "en-US"
+    ARABIC = "ar-XA"
+    FRANCE = "fr-FR"
+    GERMANNY = "de-DE"
+    HINDI = "hi-IN"
+    JAPANESE = "ja-JP"
+
+
+class Gender(str, Enum):
+    MALE = "male"
+    FEMALE = "female"
+    NEUTRAL = "neutral"
+
+
 @app.get("/text2speech")
-async def create(prompt: str = "", gender: str = "neutral", language_code="en-US"):
-    logger.info(f"text2speec using the following prompt: {repr(prompt)}")
-    result = await text_to_speech(prompt=prompt, gender=gender, language_code=language_code)
+async def create(
+    prompt: str = "",
+    gender: Gender = Gender.NEUTRAL,
+    language_code: LangugeCode = LangugeCode.ENG_US,
+):
+    logger.info(f"creating image using the following prompt: {repr(prompt)}")
+    result = await text_to_speech(prompt=prompt, gender=gender, language_code=language_code.value)
     return result
