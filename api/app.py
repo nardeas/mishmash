2b120e5f625fc03d3b874b8a7000cdd631b6a814
Teemu Taskula <themetor@gmail.com>
2022-11-05 21:32:38 +0200

    cors

--- api/app.py
+++ api/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 logger = logging.getLogger('uvicorn')
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
